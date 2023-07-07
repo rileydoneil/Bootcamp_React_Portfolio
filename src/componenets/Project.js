c998ef5ec631a16d0e08e5951430ee875c39d103
Riley O'Neil <roneilwork@gmail.com>
2023-07-07 15:09:57 -0700

    added projects, needs some fine tuning, needs to be added to portfolio page

--- src/componenets/Project.js
+++ src/componenets/Project.js
@@ -4,12 +4,23 @@ import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import { BsFillSendFill, BsGithub } from "react-icons/bs";
 
+//for image sizing issues 
+//https://stackoverflow.com/questions/50252463/material-ui-next-setting-image-size-to-fit-a-container
 const useStyles = makeStyles((theme) => ({
   paper: {
     padding: theme.spacing(1),
     textAlign: "center",
-    color: theme.palette.text.secondary
+    color: theme.palette.text.secondary,
+    height: '',
   },
+  Media: {
+    height: '100%',
+    width: '100%',
+    objectFit: 'cover',
+  },
+  Style: {
+    fontSize: '1.5rem',
+  }
 }));
 
 function GridItem({ classes, title, g_link, d_link, img_link }) {
@@ -19,7 +30,7 @@ function GridItem({ classes, title, g_link, d_link, img_link }) {
       // From 960px wide and above, I take up 25% of the device (3/12), so 4 columns fit the screen.
       <Grid item xs={12} sm={6} md={3}>
         <Paper className={classes.paper}>{title}
-        <img src={img_link} alt={title} />
+        <img src={img_link} alt={title} className={classes.Media} />
         <a href={d_link}><BsFillSendFill /></a>
         <a href={g_link}><BsGithub /></a>
         </Paper>
--- src/componenets/Projects.js
+++ src/componenets/Projects.js
@@ -3,36 +3,42 @@ import Box from '@mui/material/Box';
 import Grid from "@material-ui/core/Grid";
 import Project from './Project';
 
+//pictures
+import NoteTaker from '../assets/images/Note-Taker.PNG';
+import CinemaSearch from '../assets/images/Cinema-Search.PNG';
+import TechBlog from '../assets/images/Tech-Blog.PNG';
+import WeatherDashboard from '../assets/images/Weather-Dashboard.png';
+
 const Projects = () => {
 
     const projects = [
         {
             id: 1,
-            title: "PWA Text Editor",
+            title: "Note Taking with Express",
             g_link: "https://github.com/rileydoneil/Bootcamp_PWA_Text_Editor",
-            d_link: "",
-            img_link: '../assets/images/Note-Taker.png'
+            d_link: "https://notetakingapp1212.herokuapp.com/",
+            img_link: NoteTaker
         },
         {
             id: 2,
-            title: "Social Network API",
+            title: "Tech Blog",
             g_link: "https://github.com/rileydoneil/Bootcamp_Social_Network_API",
-            d_link: "",
-            img_link: "../assets/images/image 2.jpg"
+            d_link: "https://mvc-tech-blog-bootcamp-x-d0540789d98c.herokuapp.com/",
+            img_link: TechBlog
         },
         {
             id: 3,
-            title: "ORM E-Commerce Back End",
-            g_link: "https://github.com/rileydoneil/Bootcamp_ORM_ECommerce",
-            d_link: "",
-            img_link: "../assets/images/image 3.jpg"
+            title: "Weather Dashboard",
+            g_link: "https://github.com/rileydoneil/Weather_Dashboard",
+            d_link: "https://rileydoneil.github.io/Weather_Dashboard/",
+            img_link: WeatherDashboard
         },
         {
             id: 4,
             title: "Cinema Search",
             g_link: "https://github.com/rileydoneil/CinemaSearch",
-            d_link: "",
-            img_link: "./assets/images/image 4.jpg"
+            d_link: "https://jdempe.github.io/CinemaSearch/",
+            img_link: CinemaSearch
         }
     ];
 
